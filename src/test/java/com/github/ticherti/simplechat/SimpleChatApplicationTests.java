c40cbe5c996a25c16c676ec107581a91e2bb2317
Ticherti <vera.karr@gmail.com>
2021-11-30 23:27:40 +0300

    Initial commit

---
+++ src/test/java/com/github/ticherti/simplechat/SimpleChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ticherti.simplechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
