4749f949f5d5aae95966ba082fcbed33e6bf3b6c
Ticherti <vera.karr@gmail.com>
2022-01-12 18:17:39 +0300

    Add sql db population

--- src/main/java/com/github/ticherti/simplechat/SimpleChatApplication.java
+++ src/main/java/com/github/ticherti/simplechat/SimpleChatApplication.java
@@ -1,49 +1,11 @@
 package com.github.ticherti.simplechat;
 
-import com.github.ticherti.simplechat.entity.Message;
-import com.github.ticherti.simplechat.entity.Role;
-import com.github.ticherti.simplechat.entity.Room;
-import com.github.ticherti.simplechat.entity.User;
-import com.github.ticherti.simplechat.repository.MessageRepository;
-import com.github.ticherti.simplechat.repository.RoomRepository;
-import com.github.ticherti.simplechat.repository.UserRepository;
-import com.github.ticherti.simplechat.service.MessageService;
-import com.github.ticherti.simplechat.service.RoomService;
-import com.github.ticherti.simplechat.service.UserService;
-import com.github.ticherti.simplechat.to.SaveRequestUserDTO;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 @SpringBootApplication
 public class SimpleChatApplication implements CommandLineRunner {
-    private static final Logger log = LoggerFactory.getLogger(SimpleChatApplication.class);
-    UserRepository userRepository;
-    MessageRepository messageRepository;
-    RoomRepository roomRepository;
-
-    UserService userService;
-    MessageService messageService;
-    RoomService roomService;
-
-    public SimpleChatApplication(UserRepository userRepository, MessageRepository messageRepository,
-                                 RoomRepository roomRepository, UserService userService, MessageService messageService,
-                                 RoomService roomService) {
-        this.userRepository = userRepository;
-        this.messageRepository = messageRepository;
-        this.roomRepository = roomRepository;
-        this.userService = userService;
-        this.messageService = messageService;
-        this.roomService = roomService;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(SimpleChatApplication.class, args);
@@ -51,87 +13,6 @@ public class SimpleChatApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        basicSave();
-//        saveUsers();
     }
 
-    @Transactional
-    public void basicSave() {
-        User user1 = new User();
-        user1.setLogin("login");
-        user1.setPassword("$2a$12$UuhbNaOleqvVGc66J22l5eXVzazATWQGQfnnS2Dhsifx5b7Ox.7xG");
-        user1.setRole(Role.MODERATOR);
-        user1.setActive(true);
-        User user2 = new User();
-        user2.setLogin("login2");
-        user2.setPassword("password2");
-        user2.setRole(Role.MODERATOR);
-        user2.setActive(true);
-        SaveRequestUserDTO user3 = new SaveRequestUserDTO("login3", "password3", Role.ADMINISTRATOR);
-
-
-        Room room = new Room();
-        room.setCreator(user1);
-        room.setName("The Room2 and test");
-        room.setPrivate(false);
-        Room room3 = new Room();
-        room3.setCreator(user2);
-        room3.setName("The Room3");
-        room3.setPrivate(false);
-
-
-        List<User> users = new ArrayList<>();
-        users.add(user1);
-        users.add(user2);
-
-        List<Room> rooms = new ArrayList<>();
-        rooms.add(room);
-        rooms.add(room3);
-
-        room.setUsers(Collections.singletonList(user1));
-        room3.setUsers(users);
-//        user1.setRooms(Collections.singletonList(room));
-//        user2.setRooms(rooms);
-
-        Message message1 = new Message();
-        message1.setContent("content all content");
-        message1.setUser(user1);
-        message1.setRoom(room);
-        Message message2 = new Message();
-        message2.setContent("content all content and more");
-        message2.setUser(user1);
-        message2.setRoom(room3);
-        Message message3 = new Message();
-        message3.setContent("content all content and more for message3");
-        message3.setUser(user2);
-        message3.setRoom(room);
-        userRepository.save(user1);
-        userRepository.save(user2);
-        roomRepository.save(room3);
-        roomRepository.save(room);
-        messageRepository.save(message1);
-        messageRepository.save(message2);
-        messageRepository.save(message3);
-
-        userService.save(user3);
-
-
-//        userRepository.findAll().forEach(System.out::println);
-        System.out.println("__________");
-//        roomRepository.findAll().forEach(System.out::println);
-//        System.out.println("_______deleting________");
-//        System.out.println(userRepository.findById(1L));
-////        userRepository.deleteById(1L);
-//        System.out.println(userRepository.findById(1L));
-    }
-
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public void saveUsers() {
-
-        Room room = roomRepository.getById(3L);
-        User user = userRepository.findAll().stream().findFirst().get();
-        room.setUsers(Collections.singletonList(user));
-        roomRepository.save(room);
-        log.info("user is saved for the room");
-    }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,21 @@
+INSERT INTO users (login, password, role)
+VALUES ('login', '$2a$12$UuhbNaOleqvVGc66J22l5eXVzazATWQGQfnnS2Dhsifx5b7Ox.7xG', 'ADMINISTRATOR'),
+       ('login2', 'password2', 'MODERATOR'),
+       ('login3', '$2a$12$YrLwsgf2uUN60ufyxwjsluQ9OoU69yqkeN9tXCRpXAOPNzZtUrPlS', 'USER');
+
+INSERT INTO rooms (is_private, name, creator_id)
+VALUES ('false', 'Room1', 2),
+       ('false', 'Room2', 1);
+
+INSERT INTO messages (content, room_id, user_id)
+VALUES ('Hey, you', 1, 1),
+       ('I am here in the cold, getting lonely, getting cold', 1, 2),
+       ('Can you feel me?', 1, 1),
+       ('How is first there?', 2, 2),
+       ('I am a big strong she-wolf, how awesome my huge paws!', 2, 1),
+       ('Admin password for "login" is "password"', 2, 1);
+
+INSERT INTO rooms_users (room_id, user_id)
+VALUES (2, 1),
+       (2, 2),
+       (1, 1);
